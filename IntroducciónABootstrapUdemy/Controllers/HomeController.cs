eef8292b783bb5655696251142668293dd6c8886
gdquevedo <gdquevedo@gmail.com>
2023-12-23 14:35:27 -0600

    Primer Commit

---
+++ IntroducciónABootstrapUdemy/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using IntroducciónABootstrapUdemy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IntroducciónABootstrapUdemy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {            
+            ViewBag.Edad = 78;
+            return View("Index", "Gustavo Delgado");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IntroducciónABootstrapUdemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroducciónABootstrapUdemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
